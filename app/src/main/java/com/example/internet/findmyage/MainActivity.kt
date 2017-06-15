b9f0d8b4879acba9a121e1c51d0754d93ad74b39
InternetPC <Internet@InternetServer.com>
2017-06-15 13:44:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/internet/findmyage/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.internet.findmyage
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       /* btnAge.setOnClickListener ({
+        })*/
+    }
+
+    fun BtnClickedEevent(view: View){
+        val userDOB:String=DOBedit.text.toString()
+        val year=Calendar.getInstance().get(Calendar.YEAR)
+        val myAge=year-userDOB.toInt()
+        AgetextView.text="Your Age is "+myAge.toString()
+    }
+}
